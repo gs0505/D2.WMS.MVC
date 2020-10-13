b77c36bb105da83947d2407fe8f43ab964cd8424
Administrator <Administrator@DESKTOP-UNAQLM1>
2020-10-13 15:25:18 +0800

    首次提交

---
+++ D2-WMS-MVC/Controllers/CenterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace D2_WMS_MVC.Controllers
+{
+    public class CenterController : Controller
+    {
+        private IConfiguration _configuration;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public CenterController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public IActionResult Show()
+        {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
+            return View();
+        }
+    }
+}
